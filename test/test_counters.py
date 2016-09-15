63ed803f683a881822cb8fe1f966f28128ee5a2a
teor <teor2345@gmail.com>
2016-09-15 12:36:28 +1000

    Refactor test_counters.py to allow for multiple runs

--- test/test_counters.py
+++ test/test_counters.py
@@ -2,6 +2,10 @@
 
 from privcount.util import SecureCounters
 
+# This maximum must be kept the same as privcount's configured q value
+q = 2147483647
+
+# A simple set of byte counters
 counters = {
   'SanityCheck': {
     'bins':
@@ -23,53 +27,70 @@ counters = {
   }
 }
 
-q = 2147483647
+# create the counters for a data collector, who will generate the shares and
+# noise
+# uses q to generate the appropriate blinding factors
+# returns a tuple containing a list of DCs and a list of SKs
+def create_counters(counters, q):
+    sc_dc = SecureCounters(counters, q)
+    sc_dc.generate(['sk1', 'sk2'], 1.0)
+    # get the shares used to init the secure counters on the share keepers
+    shares = sc_dc.detach_blinding_shares()
 
-# create the counter for a data collector, who will generate the shares and noise
-sc_dc = SecureCounters(counters, q)
-sc_dc.generate(['sk1', 'sk2'], 1.0)
-# get the shares used to init the secure counters on the share keepers
-shares = sc_dc.detach_blinding_shares()
+    # create share keeper versions of the counters
+    sc_sk1 = SecureCounters(counters, q)
+    sc_sk1.import_blinding_share(shares['sk1'])
+    sc_sk2 = SecureCounters(counters, q)
+    sc_sk2.import_blinding_share(shares['sk2'])
+    return ([sc_dc], [sc_sk1, sc_sk2])
 
-# create share keeper versions of the counters
-sc_sk1 = SecureCounters(counters, q)
-sc_sk1.import_blinding_share(shares['sk1'])
-sc_sk2 = SecureCounters(counters, q)
-sc_sk2.import_blinding_share(shares['sk2'])
-
-# do some increments at the dc
-N = 500.0
-for _ in xrange(int(N)):
-    # bin[0]
-    sc_dc.increment('Bytes', 0.0)
-    sc_dc.increment('Bytes', 511.0)
+# do N increments at the dc, some bins get more increments than others
+def increment_counters(dc_list, N):
+    sc_dc = dc_list[0]
+    for _ in xrange(int(N)):
+        # bin[0]
+        sc_dc.increment('Bytes', 0.0)
+        sc_dc.increment('Bytes', 511.0)
     #bin[1]
-    sc_dc.increment('Bytes', 600.0)
+        sc_dc.increment('Bytes', 600.0)
     #bin[2]
-    sc_dc.increment('Bytes', 1024.0)
-    sc_dc.increment('Bytes', 2047.0)
+        sc_dc.increment('Bytes', 1024.0)
+        sc_dc.increment('Bytes', 2047.0)
     #bin[3]
-    pass
+        pass
     #bin[4]
     sc_dc.increment('Bytes', 4096.0)
     sc_dc.increment('Bytes', 10000.0)
 
-# get all of the counts, send for tallying
-counts_dc = sc_dc.detach_counts()
-counts_sk1 = sc_sk1.detach_counts()
-counts_sk2 = sc_sk2.detach_counts()
+# Sums the counters in dc_list and sk_list, with maximum count q
+# Returns a tallies object populated with the resulting counts
+def sum_counters(counters, q, dc_list, sk_list):
+    # get all of the counts, send for tallying
+    counts_dc_list = [sc_dc.detach_counts() for sc_dc in dc_list]
+    counts_sk_list = [sc_sk.detach_counts() for sc_sk in sk_list]
 
-# tally them up
-sc_ts = SecureCounters(counters, q)
-is_tally_success = sc_ts.tally_counters([counts_dc, counts_sk1, counts_sk2])
-assert is_tally_success
+    # tally them up
+    sc_ts = SecureCounters(counters, q)
+    counts_list = counts_dc_list + counts_sk_list
+    is_tally_success = sc_ts.tally_counters(counts_list)
+    assert is_tally_success
+    return sc_ts.detach_counts()
 
-tallies = sc_ts.detach_counts()
-print tallies
-assert tallies['Bytes']['bins'][0][2] == 2.0*N
-assert tallies['Bytes']['bins'][1][2] == 1.0*N
-assert tallies['Bytes']['bins'][2][2] == 2.0*N
-assert tallies['Bytes']['bins'][3][2] == 0.0*N
-assert tallies['Bytes']['bins'][4][2] == 2.0*N
-assert tallies['SanityCheck']['bins'][0][2] == 0.0
-print "all counts are correct!"
+# Checks that the tallies are the expected values, based on the number of
+# repetitions N
+def check_counters(tallies, N):
+    print tallies
+    assert tallies['Bytes']['bins'][0][2] == 2.0*N
+    assert tallies['Bytes']['bins'][1][2] == 1.0*N
+    assert tallies['Bytes']['bins'][2][2] == 2.0*N
+    assert tallies['Bytes']['bins'][3][2] == 0.0*N
+    assert tallies['Bytes']['bins'][4][2] == 2.0*N
+    assert tallies['SanityCheck']['bins'][0][2] == 0.0
+    print "all counts are correct!"
+
+# Check that secure counters increment correctly for small values of N
+N = 500.0
+(dc_list, sk_list) = create_counters(counters, q)
+increment_counters(dc_list, N)
+tallies = sum_counters(counters, q, dc_list, sk_list)
+check_counters(tallies, N)
